23fea6606ce471d16b7b6e4ed9bda274fe744bf8
Nikunj Goel <goel_nikunj@hotmail.com>
2020-03-05 12:59:18 +0530

    Report Config

--- edsts_automation/src/test/java/runner/TestRunner.java
+++ edsts_automation/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import stepDefinition.Hooks;
 
 
 @CucumberOptions(features = {"src/test/resources/features/CreateEntity.feature"}, glue = "stepDefinition",monochrome = true , tags = {"@CreateEntity"}, dryRun = false)
-public class TestRunner  extends AbstractTestNGCucumberTests{
-	//public class TestRunner extends Hooks {
+//public class TestRunner  extends AbstractTestNGCucumberTests{
+	public class TestRunner extends Hooks {
 
 }; 
--- edsts_automation/src/test/java/stepDefinition/Hooks.java
+++ edsts_automation/src/test/java/stepDefinition/Hooks.java
@@ -1,19 +1,29 @@
 package stepDefinition;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import helper.Config;
-
+import helper.ExtentManager;
 public class Hooks extends AbstractTestNGCucumberTests {
 	public static String browsers;
-
+	ExtentTest test;
 	@Parameters({ "browser","env" ,"siteUrl"})
 	@BeforeTest
 	public void setUpScenario(String browser, String env, String siteUrl) {
 		browsers = browser;
 		Config.Environment = env;
 		Config.siteUrl = siteUrl;
+	}
+	
+	@AfterTest
+	public void generateReport() {
+		ExtentManager._extent.endTest(test);
+		ExtentManager._extent.flush();
 	}
 }
